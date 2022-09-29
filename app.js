78ce9a0f5818832b7ad3fbd847d913a78dd06086
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-09-29 10:57:22 -0600

    added array of phrases
    -function to display a phrase
    -function to display new phrase

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
-const aButton = document.getElementById("a");
-let phrase = document.getElementById("hangman-phrase");
+//need to create array of words or phrases
+//when a letter is pressed, need to search for word in the selected word or phrase
+//must be able to detect how many wrong guesses there are
+//must display different phases of hangman image
+//have a game over screen when lives are used up
 
-aButton.addEventListener("click", pressA);
+let hangmanPhrases = [
+	"love",
+	"fortnite",
+	"apex predator",
+	"calisthenics",
+	"big brain",
+	"furious lion",
+	"kittens",
+	"basil",
+	"homemade pizza",
+	"contagious laughter",
+];
 
-function pressA() {
-    if (phrase.innerHTML = "hangman") {
-        phrase.style.display = 'block';
-    } 
+//function to display random phrase
+
+function randomPhrase(array) {
+    //get random index value
+    const randomIndex = Math.floor(Math.random() * array.length);
+    //get random item in array
+    const item = array[randomIndex];
+
+    return item;
 }
+
+const result = randomPhrase(hangmanPhrases);
+console.log(result);
+
+const currentWord = document.getElementById("hangman-phrase");
+const newWordButton = document.getElementById("new-word");
+
+console.log(currentWord);
+
+newWordButton.addEventListener("click", newWord);
+
+function insertPhrase() {
+    currentWord.innerHTML = result;
+}
+
+function newWord() {
+    location.reload();
+}
+
+insertPhrase();
