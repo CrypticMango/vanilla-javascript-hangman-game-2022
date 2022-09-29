987141f8dacf0e9f1ed7d62115044e3dc4f450f2
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-09-29 11:52:51 -0600

    spacing

--- app.js
+++ app.js
@@ -84,23 +84,23 @@ let letters = [
 
 //letter a
 function checkLetterA() {
-    if (result.includes("a")) {
-        alert("yes! the letter a is in this word");
-    } else {
-        alert("letter a is not in word")
-    }
+	if (result.includes("a")) {
+		alert("yes! the letter a is in this word");
+	} else {
+		alert("letter a is not in word");
+	}
 }
 
 let aButton = document.getElementById("a");
 aButton.addEventListener("click", checkLetterA);
 
-//letter b 
+//letter b
 function checkLetterB() {
-    if (result.includes("b")) {
-        alert("yes! the b letter is in this word");
-    } else {
-        alert("letter b is not in word")
-    }
+	if (result.includes("b")) {
+		alert("yes! the b letter is in this word");
+	} else {
+		alert("letter b is not in word");
+	}
 }
 
 let bButton = document.getElementById("b");
@@ -108,23 +108,23 @@ bButton.addEventListener("click", checkLetterB);
 
 //letter c
 function checkLetterC() {
-    if (result.includes("c")) {
-        alert("yes! the c letter is in this word");
-    } else {
-        alert("letter c is not in word")
-    }
+	if (result.includes("c")) {
+		alert("yes! the c letter is in this word");
+	} else {
+		alert("letter c is not in word");
+	}
 }
 
 let cButton = document.getElementById("c");
 cButton.addEventListener("click", checkLetterC);
 
-//letter d 
+//letter d
 function checkLetterD() {
-    if (result.includes("d")) {
-        alert("yes! the d letter is in this word");
-    } else {
-        alert("letter d is not in word")
-    }
+	if (result.includes("d")) {
+		alert("yes! the d letter is in this word");
+	} else {
+		alert("letter d is not in word");
+	}
 }
 
 let dButton = document.getElementById("d");
@@ -132,11 +132,11 @@ dButton.addEventListener("click", checkLetterD);
 
 //letter e
 function checkLetterE() {
-    if (result.includes("e")) {
-        alert("yes! the e letter is in this word");
-    } else {
-        alert("letter e is not in word")
-    }
+	if (result.includes("e")) {
+		alert("yes! the e letter is in this word");
+	} else {
+		alert("letter e is not in word");
+	}
 }
 
 let eButton = document.getElementById("e");
@@ -144,35 +144,35 @@ eButton.addEventListener("click", checkLetterE);
 
 //letter f
 function checkLetterF() {
-    if (result.includes("f")) {
-        alert("yes! the f letter is in this word");
-    } else {
-        alert("letter f is not in word")
-    }
+	if (result.includes("f")) {
+		alert("yes! the f letter is in this word");
+	} else {
+		alert("letter f is not in word");
+	}
 }
 
 let fButton = document.getElementById("f");
 fButton.addEventListener("click", checkLetterF);
 
-//letter g 
+//letter g
 function checkLetterG() {
-    if (result.includes("g")) {
-        alert("yes! the g letter is in this word");
-    } else {
-        alert("letter g is not in word")
-    }
+	if (result.includes("g")) {
+		alert("yes! the g letter is in this word");
+	} else {
+		alert("letter g is not in word");
+	}
 }
 
 let gButton = document.getElementById("g");
 gButton.addEventListener("click", checkLetterG);
 
-//letter h 
+//letter h
 function checkLetterH() {
-    if (result.includes("h")) {
-        alert("yes! the h letter is in this word");
-    } else {
-        alert("letter h is not in word")
-    }
+	if (result.includes("h")) {
+		alert("yes! the h letter is in this word");
+	} else {
+		alert("letter h is not in word");
+	}
 }
 
 let hButton = document.getElementById("h");
@@ -180,11 +180,11 @@ hButton.addEventListener("click", checkLetterH);
 
 //letter i
 function checkLetterI() {
-    if (result.includes("i")) {
-        alert("yes! the i letter is in this word");
-    } else {
-        alert("letter i is not in word")
-    }
+	if (result.includes("i")) {
+		alert("yes! the i letter is in this word");
+	} else {
+		alert("letter i is not in word");
+	}
 }
 
 let iButton = document.getElementById("i");
@@ -192,23 +192,23 @@ iButton.addEventListener("click", checkLetterI);
 
 //letter j
 function checkLetterJ() {
-    if (result.includes("j")) {
-        alert("yes! the j letter is in this word");
-    } else {
-        alert("letter j is not in word")
-    }
+	if (result.includes("j")) {
+		alert("yes! the j letter is in this word");
+	} else {
+		alert("letter j is not in word");
+	}
 }
 
 let jButton = document.getElementById("j");
 jButton.addEventListener("click", checkLetterJ);
 
-//letter k 
+//letter k
 function checkLetterK() {
-    if (result.includes("k")) {
-        alert("yes! the k letter is in this word");
-    } else {
-        alert("letter k is not in word")
-    }
+	if (result.includes("k")) {
+		alert("yes! the k letter is in this word");
+	} else {
+		alert("letter k is not in word");
+	}
 }
 
 let kButton = document.getElementById("k");
@@ -216,35 +216,35 @@ kButton.addEventListener("click", checkLetterK);
 
 //letter l
 function checkLetterL() {
-    if (result.includes("l")) {
-        alert("yes! the l letter is in this word");
-    } else {
-        alert("letter l is not in word")
-    }
+	if (result.includes("l")) {
+		alert("yes! the l letter is in this word");
+	} else {
+		alert("letter l is not in word");
+	}
 }
 
 let lButton = document.getElementById("l");
 lButton.addEventListener("click", checkLetterL);
 
-//letter m 
+//letter m
 function checkLetterM() {
-    if (result.includes("m")) {
-        alert("yes! the m letter is in this word");
-    } else {
-        alert("letter m is not in word")
-    }
+	if (result.includes("m")) {
+		alert("yes! the m letter is in this word");
+	} else {
+		alert("letter m is not in word");
+	}
 }
 
 let mButton = document.getElementById("m");
 mButton.addEventListener("click", checkLetterM);
 
-//letter n 
+//letter n
 function checkLetterN() {
-    if (result.includes("n")) {
-        alert("yes! the n letter is in this word");
-    } else {
-        alert("letter n is not in word")
-    }
+	if (result.includes("n")) {
+		alert("yes! the n letter is in this word");
+	} else {
+		alert("letter n is not in word");
+	}
 }
 
 let nButton = document.getElementById("n");
@@ -252,11 +252,11 @@ nButton.addEventListener("click", checkLetterN);
 
 //letter o
 function checkLetterO() {
-    if (result.includes("o")) {
-        alert("yes! the o letter is in this word");
-    } else {
-        alert("letter o is not in word")
-    }
+	if (result.includes("o")) {
+		alert("yes! the o letter is in this word");
+	} else {
+		alert("letter o is not in word");
+	}
 }
 
 let oButton = document.getElementById("o");
@@ -264,11 +264,11 @@ oButton.addEventListener("click", checkLetterO);
 
 //letter p
 function checkLetterP() {
-    if (result.includes("p")) {
-        alert("yes! the p letter is in this word");
-    } else {
-        alert("letter p is not in word")
-    }
+	if (result.includes("p")) {
+		alert("yes! the p letter is in this word");
+	} else {
+		alert("letter p is not in word");
+	}
 }
 
 let pButton = document.getElementById("p");
@@ -276,23 +276,23 @@ pButton.addEventListener("click", checkLetterP);
 
 //letter q
 function checkLetterQ() {
-    if (result.includes("q")) {
-        alert("yes! the q letter is in this word");
-    } else {
-        alert("letter q is not in word")
-    }
+	if (result.includes("q")) {
+		alert("yes! the q letter is in this word");
+	} else {
+		alert("letter q is not in word");
+	}
 }
 
 let qButton = document.getElementById("q");
 qButton.addEventListener("click", checkLetterQ);
 
-//letter r 
+//letter r
 function checkLetterR() {
-    if (result.includes("r")) {
-        alert("yes! the r letter is in this word");
-    } else {
-        alert("letter r is not in word")
-    }
+	if (result.includes("r")) {
+		alert("yes! the r letter is in this word");
+	} else {
+		alert("letter r is not in word");
+	}
 }
 
 let rButton = document.getElementById("r");
@@ -300,11 +300,11 @@ rButton.addEventListener("click", checkLetterR);
 
 //letter s
 function checkLetterS() {
-    if (result.includes("s")) {
-        alert("yes! the s letter is in this word");
-    } else {
-        alert("letter s is not in word")
-    }
+	if (result.includes("s")) {
+		alert("yes! the s letter is in this word");
+	} else {
+		alert("letter s is not in word");
+	}
 }
 
 let sButton = document.getElementById("s");
@@ -312,11 +312,11 @@ sButton.addEventListener("click", checkLetterS);
 
 //letter t
 function checkLetterT() {
-    if (result.includes("t")) {
-        alert("yes! the t letter is in this word");
-    } else {
-        alert("letter t is not in word")
-    }
+	if (result.includes("t")) {
+		alert("yes! the t letter is in this word");
+	} else {
+		alert("letter t is not in word");
+	}
 }
 
 let tButton = document.getElementById("t");
@@ -324,11 +324,11 @@ tButton.addEventListener("click", checkLetterT);
 
 //letter u
 function checkLetterU() {
-    if (result.includes("u")) {
-        alert("yes! the u letter is in this word");
-    } else {
-        alert("letter u is not in word")
-    }
+	if (result.includes("u")) {
+		alert("yes! the u letter is in this word");
+	} else {
+		alert("letter u is not in word");
+	}
 }
 
 let uButton = document.getElementById("u");
@@ -336,23 +336,23 @@ uButton.addEventListener("click", checkLetterU);
 
 //letter v
 function checkLetterV() {
-    if (result.includes("v")) {
-        alert("yes! the v letter is in this word");
-    } else {
-        alert("letter v is not in word")
-    }
+	if (result.includes("v")) {
+		alert("yes! the v letter is in this word");
+	} else {
+		alert("letter v is not in word");
+	}
 }
 
 let vButton = document.getElementById("v");
 vButton.addEventListener("click", checkLetterV);
 
-//letter w 
+//letter w
 function checkLetterW() {
-    if (result.includes("w")) {
-        alert("yes! the w letter is in this word");
-    } else {
-        alert("letter w is not in word")
-    }
+	if (result.includes("w")) {
+		alert("yes! the w letter is in this word");
+	} else {
+		alert("letter w is not in word");
+	}
 }
 
 let wButton = document.getElementById("w");
@@ -360,11 +360,11 @@ wButton.addEventListener("click", checkLetterW);
 
 //letter x
 function checkLetterX() {
-    if (result.includes("x")) {
-        alert("yes! the x letter is in this word");
-    } else {
-        alert("letter x is not in word")
-    }
+	if (result.includes("x")) {
+		alert("yes! the x letter is in this word");
+	} else {
+		alert("letter x is not in word");
+	}
 }
 
 let xButton = document.getElementById("x");
@@ -372,11 +372,11 @@ xButton.addEventListener("click", checkLetterX);
 
 //letter y
 function checkLetterY() {
-    if (result.includes("y")) {
-        alert("yes! the y letter is in this word");
-    } else {
-        alert("letter y is not in word")
-    }
+	if (result.includes("y")) {
+		alert("yes! the y letter is in this word");
+	} else {
+		alert("letter y is not in word");
+	}
 }
 
 let yButton = document.getElementById("y");
@@ -384,11 +384,11 @@ yButton.addEventListener("click", checkLetterY);
 
 //letter z
 function checkLetterZ() {
-    if (result.includes("z")) {
-        alert("yes! the z letter is in this word");
-    } else {
-        alert("letter z is not in word")
-    }
+	if (result.includes("z")) {
+		alert("yes! the z letter is in this word");
+	} else {
+		alert("letter z is not in word");
+	}
 }
 
 let zButton = document.getElementById("z");
@@ -396,4 +396,3 @@ zButton.addEventListener("click", checkLetterZ);
 
 //let testButton = document.getElementById("test-button");
 //testButton.addEventListener("click", checkLetterA);
-
